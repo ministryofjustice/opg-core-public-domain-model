d9bce7bfdcdf2fc435950cbc0b72dd700a04a75b
Brett Minnie <github@bdb-studios.co.uk>
2014-03-20 16:50:06 +0000

    APOLLO-1521 Possible fix for boolean casts failing

--- src/Opg/Common/Model/Entity/Traits/ExchangeArray.php
+++ src/Opg/Common/Model/Entity/Traits/ExchangeArray.php
@@ -8,7 +8,12 @@ trait ExchangeArray {
         $classData = get_class_vars(get_class($this));
 
         foreach($classData as $key=>$value) {
-            (!isset($data[$key])) ? : $this->{$key} = $data[$key];
+            if (gettype($this->{$key}) === "boolean") {
+                (!isset($data[$key])) ? : (bool)$this->{$key} = $data[$key];
+            }
+            else {
+                (!isset($data[$key])) ? : $this->{$key} = $data[$key];
+            }
         }
 
         return $this;
