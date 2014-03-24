62ad73e84f60ca819244ca2128d6ed8368dc5998
Brett Minnie <github@bdb-studios.co.uk>
2014-03-24 12:35:44 +0000

    APOLLO-1888 Updated certificateProvider

--- src/Opg/Core/Model/Entity/CaseItem/Lpa/Party/CertificateProvider.php
+++ src/Opg/Core/Model/Entity/CaseItem/Lpa/Party/CertificateProvider.php
@@ -3,6 +3,7 @@ namespace Opg\Core\Model\Entity\CaseItem\Lpa\Party;
 
 use Opg\Common\Model\Entity\Traits\ExchangeArray;
 use Opg\Common\Model\Entity\Traits\ToArray;
+use Opg\Core\Model\Entity\CaseItem\Lpa\Traits\RelationshipToDonor;
 use Opg\Core\Model\Entity\Person\Person as BasePerson;
 use Doctrine\ORM\Mapping as ORM;
 use Zend\InputFilter\Factory as InputFactory;
@@ -14,12 +15,13 @@ use Zend\Validator\Callback;
  * @package Opg Domain Model
  *
  */
-class CertificateProvider extends BasePerson implements PartyInterface
+class CertificateProvider extends BasePerson implements PartyInterface, HasRelationshipToDonor
 {
     use ToArray {
         toArray as toTraitArray;
     }
     use ExchangeArray;
+    use RelationshipToDonor;
 
     /**
      * @ORM\Column(type = "string")
