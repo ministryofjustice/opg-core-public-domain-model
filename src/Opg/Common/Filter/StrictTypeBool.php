41c0cb4c096b7dd293c853587d2b4e10efd08c57
Jeremy Quinton <jeremyquinton@gmail.com>
2014-03-24 10:18:19 +0000

    [#APOLLO-1962] adding doc block

--- src/Opg/Common/Filter/StrictTypeBool.php
+++ src/Opg/Common/Filter/StrictTypeBool.php
@@ -4,7 +4,10 @@ namespace Opg\Common\Filter;
 
 class StrictTypeBool
 {
-
+    /**
+     * @param mixed $value
+     * @return bool
+     */
     public static function apply($value) {
         if ($value === "0" or $value === "false" or $value == false) {
                    return false;
