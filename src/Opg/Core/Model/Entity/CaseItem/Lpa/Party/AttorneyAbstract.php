1e6087c1212da2baa0d41a476ec70e0ac089aca6
Brett Minnie <github@bdb-studios.co.uk>
2014-04-10 11:51:56 +0100

    APOLLO-2304 Added abstract attorney to split out the different types

---
+++ src/Opg/Core/Model/Entity/CaseItem/Lpa/Party/AttorneyAbstract.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace Opg\Core\Model\Entity\CaseItem\Lpa\Party;
+
+
+use Opg\Common\Model\Entity\HasSystemStatusInterface;
+use Opg\Common\Model\Entity\Traits\HasSystemStatus;
+use Opg\Core\Model\Entity\CaseItem\Lpa\Traits\Company;
+use Opg\Core\Model\Entity\Person\Person as BasePerson;
+use Doctrine\Orm;
+use JMS\Serializer\Annotation\Type;
+
+
+/**
+ * Class AttorneyAbstract
+ * @package Opg\Core\Model\Entity\CaseItem\Lpa\Party
+ */
+abstract class AttorneyAbstract extends BasePerson implements HasSystemStatusInterface
+{
+    use Company;
+    use HasSystemStatus;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var string
+     * @Type("string")
+     */
+    protected $dxNumber;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var string
+     */
+    protected $dxExchange;
+
+    /**
+     * @param string $dxExchange
+     * @return AttorneyAbstract
+     */
+    public function setDxExchange($dxExchange)
+    {
+        $this->dxExchange = $dxExchange;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDxExchange()
+    {
+        return $this->dxExchange;
+    }
+
+    /**
+     * @param string $dxNumber
+     * @return AttorneyAbstract
+     */
+    public function setDxNumber($dxNumber)
+    {
+        $this->dxNumber = $dxNumber;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDxNumber()
+    {
+        return $this->dxNumber;
+    }
+
+
+}
