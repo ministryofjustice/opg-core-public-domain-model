9f077ec435c7178296824a80a41eae4b7d462073
Brett Minnie <github@bdb-studios.co.uk>
2014-04-10 11:11:17 +0100

    APOLLO-2304 Extended discriminator map;

--- src/Opg/Core/Model/Entity/Person/Person.php
+++ src/Opg/Core/Model/Entity/Person/Person.php
@@ -35,6 +35,8 @@ use Zend\InputFilter\Factory as InputFactory;
  * @ORM\DiscriminatorColumn(name="type", type="string")
  * @ORM\DiscriminatorMap({
  *     "lpa_attorney" = "Opg\Core\Model\Entity\CaseItem\Lpa\Party\Attorney",
+ *     "lpa_replacement_attorney" = "Opg\Core\Model\Entity\CaseItem\Lpa\Party\ReplacementAttorney",
+ *     "lpa_trustCompany" = "Opg\Core\Model\Entity\CaseItem\Lpa\Party\TrustCompany",
  *     "lpa_correspondent" = "Opg\Core\Model\Entity\CaseItem\Lpa\Party\Correspondent",
  *     "lpa_donor" = "Opg\Core\Model\Entity\CaseItem\Lpa\Party\Donor",
  *     "lpa_notified_person" = "Opg\Core\Model\Entity\CaseItem\Lpa\Party\NotifiedPerson",
