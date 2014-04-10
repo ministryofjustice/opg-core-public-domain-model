760bfd9618e05c7337c44d25626f3a70005dfcf7
Brett Minnie <github@bdb-studios.co.uk>
2014-04-10 14:49:01 +0100

    APOLLO-2304 Implemented TrustCorporations

---
+++ src/Opg/Core/Model/Entity/CaseItem/Lpa/Party/TrustCorporation.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Opg\Core\Model\Entity\CaseItem\Lpa\Party;
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Type;
+use Opg\Common\Model\Entity\Traits\ExchangeArray;
+use Opg\Common\Model\Entity\Traits\ToArray;
+
+class TrustCorporation extends AttorneyAbstract
+{
+    use ToArray;
+    use ExchangeArray;
+
+    const AS_ATTORNEY               = 'Attorney';
+    const AS_REPLACEMENT_ATTORNEY   = 'Replacement Attorney';
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     * @var string
+     * @Type("string")
+     */
+    protected $trustCorporationAppointedAs;
+
+    /**
+     * @param string $trustCorporationAppointedAs
+     * @return TrustCorporation
+     */
+    public function setTrustCorporationAppointedAs($trustCorporationAppointedAs)
+    {
+        $this->trustCorporationAppointedAs = $trustCorporationAppointedAs;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTrustCorporationAppointedAs()
+    {
+        return $this->trustCorporationAppointedAs;
+    }
+}
