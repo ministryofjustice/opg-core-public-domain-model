39daa2a9f9019e5b12307f4029053eeaefcf2c8c
Brett Minnie <github@bdb-studios.co.uk>
2014-05-16 12:38:55 +0100

    APPOLLO-2070 Fixed merge artifact

--- src/Opg/Core/Model/Entity/Deputyship/Deputyship.php
+++ src/Opg/Core/Model/Entity/Deputyship/Deputyship.php
@@ -2,8 +2,6 @@
 namespace Opg\Core\Model\Entity\Deputyship;
 
 use Opg\Common\Model\Entity\Traits\ToArray;
-
-use Opg\Common\Model\Entity\Traits\ToArray;
 use Opg\Common\Model\Entity\Traits\InputFilter;
 use Opg\Core\Model\Entity\CaseItem\CaseItem;
 use Doctrine\ORM\Mapping as ORM;
