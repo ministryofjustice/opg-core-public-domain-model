1e0c1b1ae5dcbcd8c807139cb26327e572e9579e
Brett Minnie <github@bdb-studios.co.uk>
2014-05-27 11:15:07 +0100

    Plugged in test for persons being the same type

--- src/Opg/Core/Model/Entity/Person/Person.php
+++ src/Opg/Core/Model/Entity/Person/Person.php
@@ -223,6 +223,9 @@ abstract class Person implements HasUidInterface, HasNotesInterface, EntityInter
      */
     public function addChild(Person $person)
     {
+        if (get_class($person) !== get_class($this)) {
+            throw new \LogicException('Parent child relationships can only be defined on the same type of person');
+        }
         if ($person === $this) {
             throw new \LogicException('Person cannot be a parent of itself.');
         }
