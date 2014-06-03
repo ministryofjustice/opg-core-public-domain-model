026d4d41dc86f35c1c69d8268ccd802329547dbf
Brett Minnie <github@bdb-studios.co.uk>
2014-06-03 15:26:42 +0100

    APOLLO-2753 Fixed broken test

--- tests/OpgTest/Core/Model/Entity/CaseItem/Lpa/LpaTest.php
+++ tests/OpgTest/Core/Model/Entity/CaseItem/Lpa/LpaTest.php
@@ -507,6 +507,7 @@ class LpaTest extends \PHPUnit_Framework_TestCase
                 'paymentRemission'                          => 0,
                 'paymentExemption'                          => 0,
                 'trustCorporationSignedAs'                  => null,
+                'noNoticeGiven'                             => null, 
             ),
             $lpa->toArrayRecursive()
         );
