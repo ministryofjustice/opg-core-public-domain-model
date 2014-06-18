a62fee3089249aa942b5ecdf85492f793806f348
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2014-06-18 12:46:38 +0100

    [APOLLO-2980] Removed class defaults
    Doctrine defaults added

--- src/Opg/Core/Model/Entity/CaseItem/CaseItem.php
+++ src/Opg/Core/Model/Entity/CaseItem/CaseItem.php
@@ -74,13 +74,13 @@ abstract class CaseItem implements EntityInterface, \IteratorAggregate, CaseItem
     protected $oldCaseId;
 
     /**
-     * @ORM\Column(type = "integer", nullable=true)
+     * @ORM\Column(type = "integer", nullable=true, options={"default"=0})
      * @var int
      * @Type("integer")
      * @Accessor(getter="getApplicationType",setter="setApplicationType")
      * @Serializer\Groups({"api-poa-list","api-task-list"})
      */
-    protected $applicationType = self::APPLICATION_TYPE_CLASSIC;
+    protected $applicationType;
     /**
      * @ORM\Column(type = "string", nullable = true)
      * @var string $title
