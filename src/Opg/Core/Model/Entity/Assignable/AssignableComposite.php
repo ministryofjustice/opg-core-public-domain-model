054eab37ba8f03180980a86377df47b1dd14e127
Brett Minnie <github@bdb-studios.co.uk>
2014-07-23 11:59:22 +0100

    APOLLO-3179 Missing type annotation

--- src/Opg/Core/Model/Entity/Assignable/AssignableComposite.php
+++ src/Opg/Core/Model/Entity/Assignable/AssignableComposite.php
@@ -12,6 +12,7 @@ use Opg\Core\Model\Entity\Deputyship\Deputyship as DeputyshipEntity;
 use Opg\Core\Model\Entity\PowerOfAttorney\PowerOfAttorney as PowerOfAttorneyEntity;
 use JMS\Serializer\Annotation\Groups;
 use JMS\Serializer\Annotation\Exclude;
+use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\ReadOnly;
 use JMS\Serializer\Annotation\Accessor;
 
