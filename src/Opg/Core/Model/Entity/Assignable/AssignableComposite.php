a47785e8b7255b71e336887fb299e667f57aa001
Brett Minnie <github@bdb-studios.co.uk>
2014-08-05 11:09:35 +0100

    APOLLO-3679 Update for displayname and grouping

--- src/Opg/Core/Model/Entity/Assignable/AssignableComposite.php
+++ src/Opg/Core/Model/Entity/Assignable/AssignableComposite.php
@@ -99,6 +99,7 @@ abstract class AssignableComposite implements IsAssignee, \IteratorAggregate
     /**
      * @ORM\ManyToMany(targetEntity="Opg\Core\Model\Entity\Assignable\Team", mappedBy="members")
      * @var ArrayCollection
+     * @Groups({"api-poa-list","api-task-list"})
      * @MaxDepth(2)
      */
     protected $teams;
