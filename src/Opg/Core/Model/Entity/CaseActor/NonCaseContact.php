7ac661adaca7ba34d775b49d5d864a828724bbdf
Brett Minnie <github@bdb-studios.co.uk>
2014-09-04 16:33:45 +0100

    APOLLO-3888 Updated annotations in non case contact

--- src/Opg/Core/Model/Entity/CaseActor/NonCaseContact.php
+++ src/Opg/Core/Model/Entity/CaseActor/NonCaseContact.php
@@ -8,7 +8,7 @@ use Opg\Common\Model\Entity\Traits\ToArray;
 use Opg\Core\Model\Entity\CaseItem\CaseItemInterface;
 use Opg\Core\Model\Entity\Person\Person as BasePerson;
 use JMS\Serializer\Annotation\Exclude;
-use JMS\Serializer\Annotation\ReadOnly;
+use JMS\Serializer\Annotation\MaxDepth;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,26 +21,13 @@ class NonCaseContact extends BasePerson
     use ToArray;
 
     /**
-     * @ORM\ManyToMany(targetEntity="Opg\Core\Model\Entity\PowerOfAttorney\PowerOfAttorney", cascade={"persist"})
-     * @ORM\JoinTable(name="person_pas",
-     *     joinColumns={@ORM\JoinColumn(name="person_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="pa_id", referencedColumnName="id")}
-     * )
-     *
      * @var ArrayCollection
-     * @MaxDepth(5)
      * @ReadOnly
      * @Exclude
      */
     protected $powerOfAttorneys = null;
 
     /**
-     * @ORM\ManyToMany(targetEntity="Opg\Core\Model\Entity\Deputyship\Deputyship", cascade={"persist"})
-     * @ORM\JoinTable(name="person_deputyships",
-     *     joinColumns={@ORM\JoinColumn(name="person_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="deputyship_id", referencedColumnName="id")}
-     * )
-     * @MaxDepth(5)
      * @var ArrayCollection
      * @ReadOnly
      * @Exclude
