bf3e1fd64e903f989aec8a77e75d4cc5ebfede91
Jianzhong Yu <github@jianzhong.co.uk>
2014-09-10 10:16:01 +0100

    APOLLO-3003: add accessor methords for noticeGivenDate in NotifiedAttorney and NotifiedRelative

--- src/Opg/Core/Model/Entity/CaseActor/NotifiedAttorney.php
+++ src/Opg/Core/Model/Entity/CaseActor/NotifiedAttorney.php
@@ -5,6 +5,7 @@ namespace Opg\Core\Model\Entity\CaseActor;
 
 use Opg\Core\Model\Entity\CaseItem\Lpa\Party\Attorney;
 use Doctrine\ORM\Mapping as ORM;
+use Opg\Common\Model\Entity\DateFormat as OPGDateFormat;
 
 /**
  * @ORM\Entity
@@ -20,4 +21,53 @@ class NotifiedAttorney extends Attorney
      */
     protected $noticeGivenDate;
     
+    /**
+     * @param \DateTime $noticeGivenDate
+     *
+     * @return $this
+     */
+    public function setEpaNotifiedAttorneyNoticeGivenDate(\DateTime $noticeGivenDate = null)
+    {
+        if (is_null($noticeGivenDate)) {
+            $noticeGivenDate = new \DateTime();
+        }
+        $this->noticeGivenDate = $noticeGivenDate;
+
+        return $this;
+    }
+
+    /**
+     * @param string $noticeGivenDate
+     *
+     * @return Epa
+     */
+    public function setEpaNotifiedAttorneyNoticeGivenDateString($noticeGivenDate)
+    {
+        if (!empty($noticeGivenDate)) {
+            $noticeGivenDate = OPGDateFormat::createDateTime($noticeGivenDate);
+            $this->setEpaNotifiedAttorneyNoticeGivenDate($noticeGivenDate);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEpaNotifiedAttorneyNoticeGivenDate()
+    {
+        return $this->noticeGivenDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEpaNotifiedAttorneyNoticeGivenDateString()
+    {
+        if (!empty($this->noticeGivenDate)) {
+            return $this->noticeGivenDate->format(OPGDateFormat::getDateFormat());
+        }
+
+        return '';
+    }
 }
--- src/Opg/Core/Model/Entity/CaseActor/NotifiedRelative.php
+++ src/Opg/Core/Model/Entity/CaseActor/NotifiedRelative.php
@@ -6,6 +6,7 @@ namespace Opg\Core\Model\Entity\CaseActor;
 use Opg\Core\Model\Entity\CaseItem\Lpa\Party\HasRelationshipToDonor;
 use Opg\Core\Model\Entity\CaseItem\Lpa\Traits\RelationshipToDonor;
 use Doctrine\ORM\Mapping as ORM;
+use Opg\Common\Model\Entity\DateFormat as OPGDateFormat;
 
 /**
  * @ORM\Entity
@@ -22,4 +23,54 @@ class NotifiedRelative extends NonCaseContact implements HasRelationshipToDonor
      * @Groups("api-task-list")
      */
     protected $noticeGivenDate;
+    
+    /**
+     * @param \DateTime $noticeGivenDate
+     *
+     * @return $this
+     */
+    public function setEpaNotifiedRelativeNoticeGivenDate(\DateTime $noticeGivenDate = null)
+    {
+        if (is_null($noticeGivenDate)) {
+            $noticeGivenDate = new \DateTime();
+        }
+        $this->noticeGivenDate = $noticeGivenDate;
+
+        return $this;
+    }
+
+    /**
+     * @param string $noticeGivenDate
+     *
+     * @return Epa
+     */
+    public function setEpaNotifiedRelativeNoticeGivenDateString($noticeGivenDate)
+    {
+        if (!empty($noticeGivenDate)) {
+            $noticeGivenDate = OPGDateFormat::createDateTime($noticeGivenDate);
+            $this->setEpaNotifiedRelativeNoticeGivenDate($noticeGivenDate);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEpaNotifiedRelativeNoticeGivenDate()
+    {
+        return $this->noticeGivenDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEpaNotifiedRelativeNoticeGivenDateString()
+    {
+        if (!empty($this->noticeGivenDate)) {
+            return $this->noticeGivenDate->format(OPGDateFormat::getDateFormat());
+        }
+
+        return '';
+    }
 }
