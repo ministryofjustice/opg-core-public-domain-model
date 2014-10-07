12abe78bc2bf3bac0be13a7ff07525a4c11f2e1b
Brett Minnie <github@bdb-studios.co.uk>
2014-10-07 13:20:47 +0100

    APOLLO-4181 PaymentType complete

---
+++ tests/OpgTest/Core/Model/Entity/Payment/ChequePaymentTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brettm
+ * Date: 07/10/14
+ * Time: 12:50
+ */
+
+namespace OpgTest\Core\Model\Entity\Payment;
+
+
+class ChequePaymentTest extends \PHPUnit_Framework_TestCase {
+
+}
