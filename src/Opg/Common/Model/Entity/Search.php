522eedc5403950ae944cbe7a630e434200fde04a
Brett Minnie <github@bdb-studios.co.uk>
2014-12-02 16:40:36 +0000

    SDV-213 Updated input filter type

--- src/Opg/Common/Model/Entity/Search.php
+++ src/Opg/Common/Model/Entity/Search.php
@@ -1,6 +1,7 @@
 <?php
 namespace Opg\Common\Model\Entity;
 
+use Opg\Common\Filter\BaseInputFilter;
 use Opg\Common\Model\Entity\Traits\InputFilter as InputFilterTrait;
 use Opg\Common\Model\Entity\Traits\IteratorAggregate;
 use Opg\Common\Model\Entity\Traits\ToArray;
@@ -37,7 +38,7 @@ class Search implements EntityInterface, \IteratorAggregate
     }
 
     /**
-     * @return \Zend\InputFilter\InputFilterInterface
+     * @return BaseInputFilter
      */
     public function getInputFilter()
     {
@@ -68,7 +69,7 @@ class Search implements EntityInterface, \IteratorAggregate
                 )
             );
 
-            $this->inputFilter = $inputFilter;
+            $this->inputFilter->merge($inputFilter);
         }
 
         return $this->inputFilter;
