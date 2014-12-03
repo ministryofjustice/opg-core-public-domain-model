795b5f49c3f920819abd58267dc11bb30119025f
Brett Minnie <github@bdb-studios.co.uk>
2014-12-03 13:50:46 +0000

    SDV-213 Made a deputy assigable so that it can have an owner

--- src/Opg/Core/Model/Entity/CaseActor/Deputy.php
+++ src/Opg/Core/Model/Entity/CaseActor/Deputy.php
@@ -6,6 +6,8 @@ namespace Opg\Core\Model\Entity\CaseActor;
 use Doctrine\ORM\Mapping as ORM;
 use Opg\Common\Model\Entity\HasStatusDate;
 use Opg\Common\Model\Entity\Traits\StatusDate;
+use Opg\Core\Model\Entity\Assignable\Assignee;
+use Opg\Core\Model\Entity\Assignable\IsAssignable;
 use Opg\Core\Model\Entity\CaseActor\Decorators\CaseRecNumber;
 use Opg\Core\Model\Entity\CaseActor\Interfaces\HasCaseRecNumber;
 use Opg\Core\Model\Entity\CaseActor\Validation\InputFilter\DeputyFilter;
@@ -16,10 +18,11 @@ use Opg\Core\Model\Entity\CaseActor\Validation\InputFilter\DeputyFilter;
  *
  * @ORM\Entity
  */
-class Deputy extends Attorney implements HasStatusDate, HasCaseRecNumber
+class Deputy extends Attorney implements HasStatusDate, HasCaseRecNumber, IsAssignable
 {
     use CaseRecNumber;
     use StatusDate;
+    use Assignee;
 
     /**
      * @ORM\Column(type="string", nullable=true)
