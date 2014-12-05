790d3485a12b241ee1663642b80fec3f345d4d98
Brett Minnie <github@bdb-studios.co.uk>
2014-12-05 10:55:35 +0000

    SDV-213 Some clarity on the merge function

--- src/Opg/Common/Filter/BaseInputFilter.php
+++ src/Opg/Common/Filter/BaseInputFilter.php
@@ -50,6 +50,8 @@ class BaseInputFilter extends InputFilter
 
     /**
      * @param InputFilter $filter
+     * This feature is coming in ZF 2.4 see
+     * https://github.com/brettminnie/zf2-documentation/blob/85394c76c3fc83541f17b7822d100d7d4ce2e748/docs/languages/en/modules/zend.input-filter.intro.rst
      */
     public function merge(InputFilter $filter)
     {
@@ -58,8 +60,10 @@ class BaseInputFilter extends InputFilter
         }
     }
 
+    /**
+     * This can be overridden in calling classes, however I wanted to  be able to instanciate this class as a base
+     */
     protected function setValidators()
     {
-
     }
 }
